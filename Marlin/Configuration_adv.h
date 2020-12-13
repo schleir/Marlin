00799c53237ed24f57182ed7334e841f00772ad7
Schleir <srikanth.doss@gmail.com>
2020-12-13 15:15:38 -0800

    Increased z current 125%

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2301,7 +2301,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       580
+    #define Z_CURRENT       725
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
